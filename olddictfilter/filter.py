cb406b2a349ba4c707176c858ec747b6dbd6a44f
singrum <103807515+singrum@users.noreply.github.com>
2022-09-13 12:48:51 +0900

    Add files via upload

--- olddictfilter/filter.py
+++ olddictfilter/filter.py
@@ -1,14 +1,21 @@
 import re
 
 reg = re.compile(r'[^가-힣ㄱ-ㅎ]')
-print(reg.findall("ㄱㄱ"))
-with open("raw_olddict.txt", "r") as f:
-    lst = [a.split("	") for a in f.readlines()]
+def get_lines():
+    with open("olddictfilter/raw_olddict.txt", "r") as f:
+        return [a.split("	") for a in f.readlines()]
 
-lst = [a for a in lst if "명사" in a[-1]]
-lst = list(map(lambda x: x[0].replace('-', ""), lst))
-lst = [a for a in lst if not reg.findall(a)]
-lst = list(set(lst))
-lst.sort()
-with open("filtered_olddict.txt", "w") as f:
-    f.write("\n".join(lst))
+def write_file(word_type):
+    lst = get_lines()
+    lst = [a for a in lst if "word_type" in a[-1]]
+    lst = list(map(lambda x: x[0].replace('-', ""), lst))
+    lst = [a for a in lst if not reg.findall(a)]
+    lst = list(set(lst))
+    lst.sort()
+    with open("olddictfilter/olddict"+word_type+".txt", "w") as f:
+        f.write("\n".join(lst))
+
+
+lst = get_lines()
+lst = list(set(map(lambda x:x[-1].strip('\n'), lst)))
+[write_file(a) for a in lst]
