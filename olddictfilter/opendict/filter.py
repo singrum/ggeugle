144da5b021c2358e1814c323f65fc7cbf4350b03
singrum <103807515+singrum@users.noreply.github.com>
2022-11-07 15:25:26 +0900

    Add files via upload
    우리말샘db

---
+++ olddictfilter/opendict/filter.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import re
+
+reg = re.compile(r'[^가-힣ㄱ-ㅎ]')
+def get_lines():
+    with open("opendict.txt", "r", encoding="UTF-8") as f:
+        return [a.split("\t") for a in f.readlines()]
+
+def write_file(word_type):
+    lst = get_lines()
+    lst = [a for a in lst if word_type == a[-1].strip("\n")]
+    lst = list(map(lambda x: x[0].replace('-', ""), lst))
+    lst = [a for a in lst if not reg.findall(a)]
+    lst = list(set(lst))
+    lst.sort()
+    with open("db\\opendict"+word_type, "w", encoding="UTF-8") as f:
+        f.write("\n".join(lst))
+
+lst = get_lines()
+lst = list(set(map(lambda x:x[-1].strip('\n'), lst)))
+[write_file(a) for a in lst]
