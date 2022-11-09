d8f1ddcee1078c91ae645622283de10a18cff2ed
singrum <103807515+singrum@users.noreply.github.com>
2022-11-09 18:37:06 +0900

    Add files via upload
    bug fix

--- opendict/filter.py
+++ opendict/filter.py
@@ -8,14 +8,14 @@ def get_lines():
 
 def write_file(word_type):
     lst = get_lines()
-    lst = [a for a in lst if word_type == a[-1].strip("\n")]
+    lst = [a for a in lst if word_type == a[1].strip("\n").strip("\r")]
     lst = list(map(lambda x: x[0].replace('-', ""), lst))
     lst = [a for a in lst if not reg.findall(a)]
     lst = list(set(lst))
     lst.sort()
-    with open("db\\opendict"+word_type, "w", encoding="UTF-8") as f:
+    with open("db\\품사\\opendict"+word_type, "w", encoding="UTF-8") as f:
         f.write("\n".join(lst))
 
 lst = get_lines()
-lst = list(set(map(lambda x:x[-1].strip('\n'), lst)))
+lst = list(set(map(lambda x:x[1].strip('\n'), lst)))
 [write_file(a) for a in lst]
