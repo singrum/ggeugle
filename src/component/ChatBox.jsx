7f650cff29933c67fd29f7e78b33698abd36cbcb
강효민 <miamiq0000@gmail.com>
2024-01-20 05:14:56 +0900

    스크롤 쉐이딩 제거

--- src/component/ChatBox.jsx
+++ src/component/ChatBox.jsx
@@ -3,17 +3,18 @@ import { useState, forwardRef, useEffect } from "react";
 
 let ChatBox = forwardRef((props, ref) => {
   const [shadow, setShadow] = useState(false)
-  useEffect(()=>{
-    setShadow(ref.current.scrollHeight - ref.current.scrollTop !== ref.current.clientHeight)
-  })
+  // useEffect(()=>{
+  //   setShadow(ref.current.scrollHeight - ref.current.scrollTop !== ref.current.clientHeight)
+  // })
   return (
     <>
     
       <div className="chat-box" 
       ref = {ref}
       
-      onScroll={e => {
-        setShadow(Math.floor(e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight) !== 0)}}>
+      // onScroll={e => {
+      //   setShadow(Math.floor(e.target.scrollHeight - e.target.scrollTop - e.target.clientHeight) !== 0)}}
+        >
         {props.children}
       </div>
       <div style = {{display : shadow ? "block" : "None"}}className="chat-box-shadow"></div>
