4c99461a220441fc23e01a30545cd71c3b904be5
hyomin <miamiq0000@gmail.com>
2024-02-16 16:36:14 +0900

    breaking = always true, 제외단어 복붙시 split

--- src/component/container/AnalysisMode.jsx
+++ src/component/container/AnalysisMode.jsx
@@ -1,203 +1,265 @@
-import { useContext } from 'react';
-import { WCcontext } from '../../context/WCcontext';
-import { useState, useRef, useCallback } from 'react';
-import CharButtonCard from '../presentation/CharButtonCard'
-import WordBox from '../presentation/WordBox'
-import CharButton from '../presentation/CharButton';
-import Search from './Search'
-import { Button } from 'react-bootstrap';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Form from 'react-bootstrap/Form';
-
+import { useContext } from "react";
+import { WCcontext } from "../../context/WCcontext";
+import { useState, useRef, useCallback } from "react";
+import CharButtonCard from "../presentation/CharButtonCard";
+import WordBox from "../presentation/WordBox";
+import CharButton from "../presentation/CharButton";
+import Search from "./Search";
+import { Button } from "react-bootstrap";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import Form from "react-bootstrap/Form";
 
 export default function AnalysisMode({ display }) {
-  const { WC, setWC } = useContext(WCcontext)
-  const [wordInput, setWordInput] = useState("")
-  const [charInput, setCharInput] = useState("")
-  const [exceptWords, setExceptWords] = useState([])
-  const [chart, setChart] = useState([])
-  const [isAnalysis, setIsAnalysis] = useState(false)
-  const [simulNum, setSimulNum] = useState(0)
-  const [simulTrail, setSimulTrail] = useState([])
-  const [engine, setEngine] = useState(0)
+  const { WC, setWC } = useContext(WCcontext);
+  const [wordInput, setWordInput] = useState("");
+  const [charInput, setCharInput] = useState("");
+  const [exceptWords, setExceptWords] = useState([]);
+  const [chart, setChart] = useState([]);
+  const [isAnalysis, setIsAnalysis] = useState(false);
+  const [simulNum, setSimulNum] = useState(0);
+  const [simulTrail, setSimulTrail] = useState([]);
+  const [engine, setEngine] = useState(0);
   const id = useRef(0);
-  const WCworker = useRef()
-  const currRunningEngine = useRef(0)
-  const onWordInputChange = e => {
+  const WCworker = useRef();
+  const currRunningEngine = useRef(0);
+  const onWordInputChange = (e) => {
     if (e.target.value[e.target.value.length - 1] !== " ") {
-      setWordInput(e.target.value)
-      return
+      setWordInput(e.target.value);
+      return;
     }
-    const inputWord = wordInput.trim()
-    setWordInput('')
-    if (inputWord.length === 0) {
-      return
+    const inputWords = wordInput.trim().split(" ");
+    setWordInput("");
+    for (let inputWord of inputWords) {
+      if (inputWord.length === 0) {
+        continue;
+      }
+      if (WC.word_list.includes(inputWord)) {
+        addExceptWord(inputWord);
+      }
     }
-    if (!WC.word_list.includes(inputWord)) {
-      return
-    }
-    addExceptWord(wordInput)
-  }
+  };
 
-  const addExceptWord = useCallback(wordText => {
-    const word = { text: wordText, id: id.current++ }
+  const addExceptWord = useCallback((wordText) => {
+    const word = { text: wordText, id: id.current++ };
 
-    setExceptWords(curr => [...curr, word])
-    setCharInput(WC.rule.tail(wordText))
-  })
+    setExceptWords((curr) => [...curr, word]);
+    setCharInput(WC.rule.tail(wordText));
+  });
 
   const analysisStart = () => {
-
     if (isAnalysis === false) {
-      setIsAnalysis(true)
-      setChart([]); setSimulTrail([]);
-      currRunningEngine.current = engine
-      const excepts = exceptWords.map(e => e.text)
-      WCworker.current = new Worker(new URL('../../worker/worker.js', import.meta.url), {
-        type: 'module'
-      });
+      setIsAnalysis(true);
+      setChart([]);
+      setSimulTrail([]);
+      currRunningEngine.current = engine;
+      const excepts = exceptWords.map((e) => e.text);
+      WCworker.current = new Worker(
+        new URL("../../worker/worker.js", import.meta.url),
+        {
+          type: "module",
+        }
+      );
 
       const data = {
         rule: WC.rule.getRuleObj(),
-        word_list: WC.word_list.filter(e => !excepts.includes(e)),
+        word_list: WC.word_list.filter((e) => !excepts.includes(e)),
         currChar: charInput,
-        action: engine === 0 ? "analysis-mcts" : "analysis-exhaustive"
-      }
+        action: engine === 0 ? "analysis-mcts" : "analysis-exhaustive",
+      };
 
-      WCworker.current.postMessage(data)
+      WCworker.current.postMessage(data);
 
       if (engine === 0) {
-
         WCworker.current.onmessage = ({ data }) => {
-          setChart(getChart(data.chart))
-          setSimulNum(data.num)
-          setSimulTrail(data.trail)
+          setChart(getChart(data.chart));
+          setSimulNum(data.num);
+          setSimulTrail(data.trail);
           if (data.num >= 1000000 || data.terminate === true) {
-
-            setIsAnalysis(false)
-            WCworker.current.terminate()
+            setIsAnalysis(false);
+            WCworker.current.terminate();
           }
-        }
-      }
-      else if (engine === 1) {
-
+        };
+      } else if (engine === 1) {
         WCworker.current.onmessage = ({ data }) => {
           if (data.word) {
-            setChart(
-              curr => [
-                ...curr,
-                <div className="chart-item" key={data.word}>
-                  <CharButton key={`${data.word}`} type="cir" strength={`${0}`}>{data.word}
-                    <img className="delete-icon" onClick={() => { if (data.word.length === 1) { setCharInput(data.word); return } addExceptWord(data.word) }} src="icon/add_FILL0_wght400_GRAD0_opsz24.svg" /></CharButton>
-                  <span className="analysis-exhaustive-result">
-                    {data.win ? "승" : "패"}
-                  </span>
-                </div>
-              ]
-            )
+            setChart((curr) => [
+              ...curr,
+              <div className="chart-item" key={data.word}>
+                <CharButton key={`${data.word}`} type="cir" strength={`${0}`}>
+                  {data.word}
+                  <img
+                    className="delete-icon"
+                    onClick={() => {
+                      if (data.word.length === 1) {
+                        setCharInput(data.word);
+                        return;
+                      }
+                      addExceptWord(data.word);
+                    }}
+                    src="icon/add_FILL0_wght400_GRAD0_opsz24.svg"
+                  />
+                </CharButton>
+                <span className="analysis-exhaustive-result">
+                  {data.win ? "승" : "패"}
+                </span>
+              </div>,
+            ]);
           }
           if (data.trail) {
-            setSimulTrail(data.trail)
+            setSimulTrail(data.trail);
           }
           if (data.terminate) {
-            setIsAnalysis(false)
-            setSimulTrail([])
-            WCworker.current.terminate()
+            setIsAnalysis(false);
+            setSimulTrail([]);
+            WCworker.current.terminate();
           }
-        }
+        };
       }
-
     }
     if (isAnalysis === true) {
-      setIsAnalysis(false)
-      WCworker.current.terminate()
+      setIsAnalysis(false);
+      WCworker.current.terminate();
     }
-
-
-  }
-  const getChart = chartData => {
+  };
+  const getChart = (chartData) => {
     if (engine === 0) {
-      const words = Object.keys(chartData).sort((a, b) => chartData[b] - chartData[a])
-      const chart = words.map(word => (<div className="chart-item" key={word}>
-        <CharButton key={`${word}`} type="cir" strength={`${0}`}>{word}
-          <img className="delete-icon" onClick={() => { if (word.length === 1) { setCharInput(word); return } addExceptWord(word) }} src="icon/add_FILL0_wght400_GRAD0_opsz24.svg" /></CharButton>
-        <span className="progress-bar-wrap">
-          <ProgressBar className="analysis-bar" now={chartData[word] * 100} label={`${Math.round(chartData[word] * 10000) / 100}%`} />
-        </span>
-      </div>))
-      return chart
+      const words = Object.keys(chartData).sort(
+        (a, b) => chartData[b] - chartData[a]
+      );
+      const chart = words.map((word) => (
+        <div className="chart-item" key={word}>
+          <CharButton key={`${word}`} type="cir" strength={`${0}`}>
+            {word}
+            <img
+              className="delete-icon"
+              onClick={() => {
+                if (word.length === 1) {
+                  setCharInput(word);
+                  return;
+                }
+                addExceptWord(word);
+              }}
+              src="icon/add_FILL0_wght400_GRAD0_opsz24.svg"
+            />
+          </CharButton>
+          <span className="progress-bar-wrap">
+            <ProgressBar
+              className="analysis-bar"
+              now={chartData[word] * 100}
+              label={`${Math.round(chartData[word] * 10000) / 100}%`}
+            />
+          </span>
+        </div>
+      ));
+      return chart;
+    } else if (engine === 1) {
     }
-    else if (engine === 1) {
+  };
 
-    }
+  return (
+    WC && (
+      <div style={{ display: display ? "block" : "none" }}>
+        <div className="analysis-setting">
+          <div className="setting-body">
+            <div className="analysis-box">
+              <div className="title">
+                제외할 단어{" "}
+                <span style={{ fontWeight: "500", color: "gray" }}>
+                  (공백으로 구분)
+                </span>
+              </div>
+              <WordBox>
+                <CharButtonCard>
+                  {exceptWords.map((word) => (
+                    <CharButton
+                      key={`${word.id}`}
+                      type="cir"
+                      strength={`${0}`}
+                      delete="true"
+                    >
+                      {word.text}
+                      <img
+                        className="delete-icon"
+                        onClick={() =>
+                          setExceptWords(
+                            exceptWords.filter((e) => e.id != word.id)
+                          )
+                        }
+                        src="icon/close_FILL1_wght400_GRAD0_opsz24.svg"
+                      />
+                    </CharButton>
+                  ))}
+                  <input
+                    className="analysis-input"
+                    type="search"
+                    placeholder="제외할 단어를 입력해주세요."
+                    value={wordInput || ""}
+                    onChange={onWordInputChange}
+                  />
+                </CharButtonCard>
+              </WordBox>
+            </div>
 
-  }
+            <div className="analysis-box">
+              <div className="title">시작 글자</div>
+              <Search input={charInput} setInput={setCharInput}></Search>
+            </div>
+            <div className="analysis-box">
+              <div className="title">분석 방법</div>
+              <Form.Select
+                aria-label="Default select example"
+                value={engine}
+                onChange={(e) => {
+                  setEngine(parseInt(e.target.value));
+                }}
+              >
+                <option value="0">MCTS</option>
+                <option value="1">완전탐색</option>
+              </Form.Select>
+            </div>
+          </div>
 
-
-  return WC && (
-    <div style={{ display: display ? "block" : "none" }}>
-      <div className='analysis-setting'>
-
-        <div className="setting-body">
-          <div className='analysis-box'>
-            <div className='title'>제외할 단어 <span style={{ fontWeight: "500", color: "gray" }}>(공백으로 구분)</span></div>
-            <WordBox>
-              <CharButtonCard>
-                {exceptWords.map((word) => (<CharButton key={`${word.id}`} type="cir" strength={`${0}`} delete="true">{word.text}<img className="delete-icon" onClick={() => setExceptWords(exceptWords.filter(e => e.id != word.id))} src="icon/close_FILL1_wght400_GRAD0_opsz24.svg" /></CharButton>))}
-                <input
-                  className="analysis-input"
-                  type="search"
-                  placeholder="제외할 단어를 입력해주세요."
-                  value={wordInput || ''}
-                  onChange={onWordInputChange}
+          <div className="setting-foot">
+            <Button
+              className="analysis-btn"
+              variant="primary"
+              onClick={analysisStart}
+            >
+              {isAnalysis ? "분석 중지" : "분석 시작"}
+              {isAnalysis ? (
+                <img
+                  className="analysis-icon"
+                  src="icon/stop_FILL1_wght400_GRAD0_opsz24.svg"
                 />
-              </CharButtonCard>
-            </WordBox>
-
+              ) : (
+                <img
+                  className="analysis-icon"
+                  src="icon/play_arrow_FILL1_wght400_GRAD0_opsz24.svg"
+                />
+              )}
+            </Button>
           </div>
-
-          <div className='analysis-box'>
-            <div className='title'>시작 글자</div>
-            <Search input={charInput} setInput={setCharInput}></Search>
-
-          </div>
-          <div className='analysis-box'>
-            <div className='title'>분석 방법</div>
-            <Form.Select aria-label="Default select example" value={engine} onChange={(e) => { setEngine(parseInt(e.target.value)); }}>
-              <option value="0">MCTS</option>
-              <option value="1">완전탐색</option>
-            </Form.Select>
-          </div>
-
         </div>
-
-        <div className='setting-foot'>
-          <Button className="analysis-btn" variant="primary" onClick={analysisStart}>
-            {isAnalysis ? "분석 중지" : "분석 시작"}
-            {isAnalysis ? (<img className="analysis-icon" src="icon/stop_FILL1_wght400_GRAD0_opsz24.svg" />) :
-              (<img className="analysis-icon" src="icon/play_arrow_FILL1_wght400_GRAD0_opsz24.svg" />)}
-
-          </Button>
+        <div className="analysis-result">
+          {currRunningEngine.current === 0 ? (
+            <>
+              <div className="analysis-num">
+                <span className="title">시뮬레이션</span> : {simulNum} 회
+              </div>
+              <div className="analysis-trail overflow">
+                {simulTrail.join(" - ")}
+              </div>
+              <div className="chart-wrap">{chart}</div>
+            </>
+          ) : (
+            <>
+              <div className="chart-wrap">{chart}</div>
+              <div className="analysis-trail">{simulTrail.join(" - ")}</div>
+            </>
+          )}
         </div>
+        <div className="padding"></div>
       </div>
-      <div className='analysis-result'>
-        {currRunningEngine.current === 0 ? 
-        (<><div className="analysis-num"><span className='title'>시뮬레이션</span> : {simulNum} 회</div>
-        <div className="analysis-trail overflow">{simulTrail.join(" - ")}</div>
-        <div className="chart-wrap">
-          {chart}
-        </div></>)
-        :(<><div className="chart-wrap">
-          {chart}
-        </div>
-        <div className="analysis-trail">{simulTrail.join(" - ")}</div></>)}
-
-
-      </div>
-      <div className='padding'></div>
-
-    </div>
-
-
-  )
+    )
+  );
 }
--- src/worker/worker.js
+++ src/worker/worker.js
@@ -1,228 +1,234 @@
-import { WCengine, Rule, MCTS, MCTSTurn, Turn,WIN, LOS, WINCIR, LOSCIR, ROUTE} from "../js/WordChain"
+import {
+  WCengine,
+  Rule,
+  MCTS,
+  MCTSTurn,
+  Turn,
+  WIN,
+  LOS,
+  WINCIR,
+  LOSCIR,
+  ROUTE,
+} from "../js/WordChain";
 
-const heuristic = (currChar, wc)=>{ 
-  if (!currChar){
-    return
+const heuristic = (currChar, wc) => {
+  if (!currChar) {
+    return;
   }
   let word;
   if (wc.charMap[currChar].sorted === WIN) {
-    let mapClass = wc.charMap[currChar].wordClass
-    let key = Math.min(...Object.keys(mapClass).filter(e => !isNaN(e) && Number(e) >= 0).map(e => Number(e)))
-    let next = mapClass[key]
-    word = next[Math.floor(Math.random() * next.length)]
-    return {word, win : true}
-  }
-  else if (wc.charMap[currChar].sorted === LOS) {
-    let mapClass = wc.charMap[currChar].wordClass
-    if(Object.keys(mapClass).length === 0){
-      return {word : undefined, win : false}  
+    let mapClass = wc.charMap[currChar].wordClass;
+    let key = Math.min(
+      ...Object.keys(mapClass)
+        .filter((e) => !isNaN(e) && Number(e) >= 0)
+        .map((e) => Number(e))
+    );
+    let next = mapClass[key];
+    word = next[Math.floor(Math.random() * next.length)];
+    return { word, win: true };
+  } else if (wc.charMap[currChar].sorted === LOS) {
+    let mapClass = wc.charMap[currChar].wordClass;
+    if (Object.keys(mapClass).length === 0) {
+      return { word: undefined, win: false };
     }
-    let key = Math.min(...Object.keys(mapClass))
-    let next = mapClass[key]
-    word = next[Math.floor(Math.random() * next.length)]
-    return {word, win : false}
-  }
-  else if (wc.charMap[currChar].sorted === LOSCIR) {
+    let key = Math.min(...Object.keys(mapClass));
+    let next = mapClass[key];
+    word = next[Math.floor(Math.random() * next.length)];
+    return { word, win: false };
+  } else if (wc.charMap[currChar].sorted === LOSCIR) {
     if (wc.charMap[currChar].wordClass["RETURN"]) {
-      let next = wc.charMap[currChar].wordClass["RETURN"]
-      word = next[Math.floor(Math.random() * next.length)]
-      return {word, win : false}
+      let next = wc.charMap[currChar].wordClass["RETURN"];
+      word = next[Math.floor(Math.random() * next.length)];
+      return { word, win: false };
     } else {
-      let next = wc.charMap[currChar].wordClass["LOSCIR"]
+      let next = wc.charMap[currChar].wordClass["LOSCIR"];
 
-      word = next[Math.floor(Math.random() * next.length)]
-      return {word, win : false}
+      word = next[Math.floor(Math.random() * next.length)];
+      return { word, win: false };
+    }
+  } else if (wc.charMap[currChar].sorted === WINCIR) {
+    word = wc.charMap[currChar].solution;
+    return { word, win: true };
+  } else {
+    let nextRoute = wc.charMap[currChar].wordClass["ROUTE"];
+    nextRoute = nextRoute.filter(
+      (word) =>
+        nextRoute.find((e) => wc.rule.tail(e) === wc.rule.tail(word)) === word
+    );
+    if (nextRoute.length === 1) {
+      word = nextRoute[0];
+      return { word, win: "ROUTE" };
     }
   }
-  else if (wc.charMap[currChar].sorted === WINCIR) {
-    word = wc.charMap[currChar].solution
-    return {word, win : true}
-  }
-  else{
-    let nextRoute = wc.charMap[currChar].wordClass["ROUTE"]
-    nextRoute = nextRoute.filter(word => nextRoute.find(e=>wc.rule.tail(e) === wc.rule.tail(word)) === word)
-    if(nextRoute.length === 1){
-      word = nextRoute[0]
-      return {word, win : "ROUTE"}
-    }
-  }
-}
+};
 
-
-const analysisMCTS = (data)=>{
+const analysisMCTS = (data) => {
   const r = new Rule(
     data.rule.changable_index,
     data.rule.head_index,
-    data.rule.tail_index,
+    data.rule.tail_index
   );
-  const wc = new WCengine(r)
-  wc.word_list = data.word_list
-  wc.update()
+  const wc = new WCengine(r);
+  wc.word_list = data.word_list;
+  wc.update();
 
-  const heuristicWord = heuristic(data.currChar, wc)
-  if(heuristicWord && heuristicWord.win !== "ROUTE"){
-    if(!heuristicWord.word){
-      self.postMessage({trail : [],chart : {}, num : 0, terminate : true})   
+  const heuristicWord = heuristic(data.currChar, wc);
+  if (heuristicWord && heuristicWord.win !== "ROUTE") {
+    if (!heuristicWord.word) {
+      self.postMessage({ trail: [], chart: {}, num: 0, terminate: true });
     }
-    
-    self.postMessage({trail : [],chart : {[heuristicWord.word] : heuristicWord.win ? 1 : 0}, num : 0, terminate : true}) 
 
-    
-    return 
+    self.postMessage({
+      trail: [],
+      chart: { [heuristicWord.word]: heuristicWord.win ? 1 : 0 },
+      num: 0,
+      terminate: true,
+    });
+
+    return;
   }
-  
 
-  wc.word_list = wc.routeWords
-  wc.update()
-  const mcts = new MCTS(new MCTSTurn(undefined, data.currChar, wc, []))
-  
-  for(let i = 0; i < 10000; i++){
-    for(let j = 0;j<100;j++){
-      mcts.learn()
+  wc.word_list = wc.routeWords;
+  wc.update();
+  const mcts = new MCTS(new MCTSTurn(undefined, data.currChar, wc, []));
+
+  for (let i = 0; i < 10000; i++) {
+    for (let j = 0; j < 100; j++) {
+      mcts.learn();
     }
-    const chart = mcts.getChart()
-    const trail = mcts.getTrail()
-    self.postMessage({chart, num : (i + 1) * 100, terminate : false, trail})  
+    const chart = mcts.getChart();
+    const trail = mcts.getTrail();
+    self.postMessage({ chart, num: (i + 1) * 100, terminate: false, trail });
   }
-  
-}
+};
 const analysisExhaustive = (data) => {
   const r = new Rule(
     data.rule.changable_index,
     data.rule.head_index,
-    data.rule.tail_index,
+    data.rule.tail_index
   );
-  const wc = new WCengine(r)
-  wc.word_list = data.word_list
-  wc.update()
+  const wc = new WCengine(r);
+  wc.word_list = data.word_list;
+  wc.update();
 
-  const heuristicWord = heuristic(data.currChar, wc)
-  if(heuristicWord && heuristicWord.win !== "ROUTE"){
-    if(!heuristicWord.word){
-      self.postMessage({chart : {}, num : 0, terminate : true})   
+  const heuristicWord = heuristic(data.currChar, wc);
+  if (heuristicWord && heuristicWord.win !== "ROUTE") {
+    if (!heuristicWord.word) {
+      self.postMessage({ chart: {}, num: 0, terminate: true });
     }
-    
-    self.postMessage({word : heuristicWord.word, win : heuristicWord.win, terminate : true}) 
 
-    
-    return 
+    self.postMessage({
+      word: heuristicWord.word,
+      win: heuristicWord.win,
+      terminate: true,
+    });
+
+    return;
   }
-  
 
-  wc.word_list = wc.routeWords
-  wc.update()
-  const stack = []
-  const currTurn = new Turn(undefined, data.currChar, wc, [])
-  const nextRoute = currTurn.nextRoute.sort((a, b) =>
-  
-  currTurn.WCengine.charMap[wc.rule.tail(a)].wordClass["ROUTE"].length -
-  currTurn.WCengine.charMap[wc.rule.tail(b)].wordClass["ROUTE"].length)
+  wc.word_list = wc.routeWords;
+  wc.update();
+  const stack = [];
+  const currTurn = new Turn(undefined, data.currChar, wc, []);
+  const nextRoute = currTurn.nextRoute.sort(
+    (a, b) =>
+      currTurn.WCengine.charMap[wc.rule.tail(a)].wordClass["ROUTE"].length -
+      currTurn.WCengine.charMap[wc.rule.tail(b)].wordClass["ROUTE"].length
+  );
+
   for (let next of nextRoute) {
-    const nextTurn = currTurn.getTurnFromRoute(next)
-    const win = !isWin(nextTurn)
-    self.postMessage({word : next, win})
+    const nextTurn = currTurn.getTurnFromRoute(next);
+    const win = !isWin(nextTurn);
+    self.postMessage({ word: next, win });
   }
-  self.postMessage({terminate : true})
+  self.postMessage({ terminate: true });
 
-  
   function isWin(turn) {
-    stack.push(turn.prevWord)
-    
+    stack.push(turn.prevWord);
+
     if (turn.nextRoute.length === 0) {
-      
-      let type = turn.WCengine.charMap[turn.currChar].sorted
+      let type = turn.WCengine.charMap[turn.currChar].sorted;
       if (type === WINCIR || type === WIN) {
-        self.postMessage({trail : stack})
-        stack.pop()
-        return true
-      }
-      else if (type === LOSCIR || type === LOS) {
-        self.postMessage({trail : stack})
-        stack.pop()
-        return false
+        self.postMessage({ trail: stack });
+        stack.pop();
+        return true;
+      } else if (type === LOSCIR || type === LOS) {
+        self.postMessage({ trail: stack });
+        stack.pop();
+        return false;
       }
     }
     // 여기서 nextRoute의 wordclass가 존재안함
-    const nextRoute = turn.nextRoute.sort((a, b) =>
-      turn.WCengine.charMap[turn.WCengine.rule.tail(a)].wordClass["ROUTE"]?.length -
-      turn.WCengine.charMap[turn.WCengine.rule.tail(b)].wordClass["ROUTE"]?.length)
+    const nextRoute = turn.nextRoute.sort(
+      (a, b) =>
+        turn.WCengine.charMap[turn.WCengine.rule.tail(a)].wordClass["ROUTE"]
+          ?.length -
+        turn.WCengine.charMap[turn.WCengine.rule.tail(b)].wordClass["ROUTE"]
+          ?.length
+    );
     for (let next of nextRoute) {
-      const nextTurn = turn.getTurnFromRoute(next)
+      const nextTurn = turn.getTurnFromRoute(next);
+
       if (!isWin(nextTurn)) {
-        self.postMessage({trail : stack})
-        stack.pop()
-        return true
+        self.postMessage({ trail: stack });
+        stack.pop();
+        return true;
       }
     }
-    self.postMessage({trail : stack})
-    stack.pop()
-    return false
+    self.postMessage({ trail: stack });
+    stack.pop();
+    return false;
   }
+};
 
-  
-  
-  
-  
-}
-
-
-const getWinWord = (data, diff)=>{
+const getWinWord = (data, diff) => {
   const r = new Rule(
     data.rule.changable_index,
     data.rule.head_index,
-    data.rule.tail_index,
+    data.rule.tail_index
   );
-  const wc = new WCengine(r)
-  wc.word_list = data.word_list
-  wc.update()
+  const wc = new WCengine(r);
+  wc.word_list = data.word_list;
+  wc.update();
 
-  const heuristicWord = heuristic(data.currChar, wc)
-  if(heuristicWord){
-    self.postMessage({word : heuristicWord.word, terminate : true})
-    return 
+  const heuristicWord = heuristic(data.currChar, wc);
+  if (heuristicWord) {
+    self.postMessage({ word: heuristicWord.word, terminate: true });
+    return;
   }
-  
+
   let word;
-  if(diff === 1){
-    const nextRoute = wc.charMap[data.currChar].wordClass["ROUTE"]
-    word = nextRoute[Math.floor(Math.random() * nextRoute.length)]
-  }
+  if (diff === 1) {
+    const nextRoute = wc.charMap[data.currChar].wordClass["ROUTE"];
+    word = nextRoute[Math.floor(Math.random() * nextRoute.length)];
+  } else if (diff === 2) {
+    wc.word_list = wc.routeWords;
+    wc.update();
 
-  else if(diff === 2){
-    wc.word_list = wc.routeWords
-    wc.update()
-    
-    const mcts = new MCTS(new MCTSTurn(undefined, data.currChar, wc, []))
-    
-    for(let j = 0;j<500;j++){
-      mcts.learn()
+    const mcts = new MCTS(new MCTSTurn(undefined, data.currChar, wc, []));
+
+    for (let j = 0; j < 500; j++) {
+      mcts.learn();
     }
-    word = mcts.getWinWord()
+    word = mcts.getWinWord();
   }
-  
-  self.postMessage({word, terminate : true})
-  
 
-  
-}
+  self.postMessage({ word, terminate: true });
+};
 
-
-
-self.onmessage = ({data}) => {
+self.onmessage = ({ data }) => {
   switch (data.action) {
-    case 'analysis-mcts':
-      analysisMCTS(data)
+    case "analysis-mcts":
+      analysisMCTS(data);
       break;
-    case 'analysis-exhaustive':
-      analysisExhaustive(data)
+    case "analysis-exhaustive":
+      analysisExhaustive(data);
       break;
-    case 'getWinWordMidium':
-      
-      getWinWord(data, 1)
+    case "getWinWordMidium":
+      getWinWord(data, 1);
       break;
-    case 'getWinWordHard':
-      getWinWord(data, 2)
+    case "getWinWordHard":
+      getWinWord(data, 2);
       break;
   }
-  
-}
+};
