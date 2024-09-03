5a3b24512fe9809adcfa99c67952bba36f7dbeeb
hyomin <miamiq0000@gmail.com>
2024-09-03 21:15:03 +0900

    디자인

--- src/pages/header/Logo.tsx
+++ src/pages/header/Logo.tsx
@@ -11,7 +11,7 @@ export default function Logo() {
       : theme.theme;
 
   return (
-    <div className="font-semibold text-lg cursor-pointer flex gap-1 items-center md:flex-col md:gap-0 lg:gap-1 lg:flex-row lg:text-xl">
+    <div className="text-lg cursor-pointer flex gap-1 items-center md:flex-col md:gap-0 lg:gap-1 lg:flex-row lg:text-xl">
       {currTheme === "dark" ? (
         <img src={DarkLogo} alt="logo" className="max-w-5 md:max-w-6" />
       ) : (
