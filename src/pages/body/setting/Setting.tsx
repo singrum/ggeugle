585fb65b5b7b67b0bbf647d20b0242085c1ef920
hyomin <miamiq0000@gmail.com>
2024-10-13 02:51:11 +0900

    style

--- src/pages/body/setting/Setting.tsx
+++ src/pages/body/setting/Setting.tsx
@@ -1,8 +1,6 @@
-import { useMediaQuery } from "@/hooks/use-media-query";
 import { RuleSetting } from "./RuleSetting";
 
 export default function Setting() {
-  const isDesktop = useMediaQuery("(min-width: 768px)");
   return (
     <div className="md:min-h-0 md:overflow-auto h-full md:h-full w-full md:flex md:justify-center">
       <div className="w-full flex flex-col gap-5 min-h-0 overflow-auto">
