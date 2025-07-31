115a0acdcacb799d9bf796774e3754dad5570cd9
hyomin <miamiq0000@gmail.com>
2025-07-31 11:24:58 +0900

    x

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import path from "path";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/ggeugle/",
+  base: "/ggeugle-old/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
