09d16fe7c028db8af0e36213f0bffe5866728cf7
hyomin <miamiq0000@gmail.com>
2025-08-17 04:17:47 +0900

    디자인

--- src/app/search/search-section/excepted-words-input-container.tsx
+++ src/app/search/search-section/excepted-words-input-container.tsx
@@ -5,15 +5,12 @@ export default function ExceptedWordsInputContainer({
   children,
 }: React.ComponentProps<"div">) {
   return (
-    <div className="relative flex flex-col items-center gap-6 px-4 py-8 md:px-6">
+    <div className="relative flex flex-col items-center gap-6 px-4 py-10 md:px-6">
       <div className="space-y-4 text-center">
         <div className="text-2xl font-bold">검색</div>
         <div className="text-muted-foreground px-4 text-sm leading-6 break-keep">
           <p>음절, 단어, 또는 기보를 검색하세요.</p>
-          <p>
-            여러 단어는 띄어쓰기로 구분하며, 구분된 단어들은 전체 목록에서
-            제외됩니다.
-          </p>
+          <p>띄어쓰기로 구분된 단어들은 단어 목록에서 제외됩니다.</p>
         </div>
       </div>
       <Card className="bg-muted flex w-full flex-col justify-end gap-0 rounded-xl py-0">
