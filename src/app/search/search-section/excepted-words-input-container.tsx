ebcc7612c81ccea81119174c568b16be9612797a
hyomin <miamiq0000@gmail.com>
2025-08-17 18:06:58 +0900

    디자인

--- src/app/search/search-section/excepted-words-input-container.tsx
+++ src/app/search/search-section/excepted-words-input-container.tsx
@@ -8,10 +8,11 @@ export default function ExceptedWordsInputContainer({
     <div className="relative flex flex-col items-center gap-6 px-4 py-10 pb-14 md:px-6">
       <div className="space-y-4 text-center">
         <div className="text-2xl font-bold">검색</div>
-        <div className="text-muted-foreground px-2 text-sm leading-6 break-keep">
-          <p>음절, 단어, 또는 기보를 검색하세요.</p>
-          <p>공백으로 구분된 단어들은 전체 단어 목록에서 제외됩니다.</p>
-        </div>
+
+        <p className="max-w-3xl px-2 text-sm leading-6 text-balance break-keep">
+          음절, 단어, 또는 기보를 검색하세요. 공백으로 구분된 단어들은 전체 단어
+          목록에서 제외됩니다.
+        </p>
       </div>
       <Card className="bg-muted flex w-full flex-col justify-end gap-0 rounded-xl py-0">
         {children}
