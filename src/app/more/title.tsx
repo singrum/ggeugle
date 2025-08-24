879420f8c274a983833e57004318793ba032b79b
hyomin <miamiq0000@gmail.com>
2025-08-24 17:57:20 +0900

    디자인 : 지식 메뉴 간격

--- src/app/more/title.tsx
+++ src/app/more/title.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { useTheme } from "@/providers/theme-provider";
 import { Slash } from "lucide-react";
 import IkkiLogoWhite from "/ikki-logo/ikki-green-dark.png";
@@ -18,11 +19,17 @@ export default function Title() {
       : theme;
   return (
     <h1 className="flex items-center gap-2">
-      <img src={logoInfo[resolvedTheme]} className="w-12" alt="이끼" />
-
+      <Button variant="link" asChild className="px-0">
+        <a href={"https://ikki.app"} target="_blank" rel="noopener noreferrer">
+          <img src={logoInfo[resolvedTheme]} className="w-12" alt="이끼" />
+        </a>
+      </Button>
       <Slash className="stroke-muted-foreground size-5 shrink-0 stroke-1" />
-
-      <div className="text-lg font-medium">끝말잇기 엔진</div>
+      <Button variant="link" asChild className="px-0">
+        <a href="/">
+          <div className="text-lg font-medium">끝말잇기 엔진</div>
+        </a>
+      </Button>
     </h1>
   );
 }
