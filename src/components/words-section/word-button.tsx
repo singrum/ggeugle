d96dea88bcea35f696bb06e02044e0d195fd4955
hyomin <miamiq0000@gmail.com>
2025-08-31 13:23:20 +0900

    글자 단위 클릭

--- src/components/words-section/word-button.tsx
+++ src/components/words-section/word-button.tsx
@@ -65,20 +65,27 @@ export function ExceptedWordButton({
 }: React.ComponentProps<"button"> & {}) {
   const search = useWcStore((e) => e.search);
   const removeExceptedWord = useWcStore((e) => e.removeExceptedWord);
-  const solver = useWcStore((e) => e.solver);
+
   return (
     <div className="group/wordbutton text-muted-foreground bg-secondary flex w-fit max-w-full cursor-default items-center gap-0 rounded-xl shadow-sm transition-colors">
-      <Button
-        onClick={() => search((children as string).at(solver!.tailIdx)!)}
-        variant="ghost"
-        className="hover/wordbutton:text-foreground h-auto flex-1 rounded-l-full py-1 pr-2 pl-3 text-left break-all whitespace-normal"
-      >
-        {children}
-      </Button>
+      <div className="hover/wordbutton:text-foreground z-1 h-auto flex-1 rounded-l-full text-left text-sm font-medium break-all whitespace-normal">
+        {(children as string).split("").map((e, i) => (
+          <Button
+            className={cn("h-fit p-0 py-1", {
+              "pl-3": i === 0,
+              "pr-2": i === (children as string).length - 1,
+            })}
+            onClick={() => search(e)}
+            variant="ghost"
+          >
+            {e}
+          </Button>
+        ))}
+      </div>
       <Button
         variant="ghost"
         size="icon"
-        className="group/wordbuttonplus -ml-2 size-8 rounded-full"
+        className="group/wordbuttonplus z-0 -ml-2 size-8 rounded-full"
         onClick={() => {
           removeExceptedWord(children as string);
         }}
