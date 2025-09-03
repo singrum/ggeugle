0331913669aa77d9d901efc8d937561d01e20de9
hyomin <miamiq0000@gmail.com>
2025-09-03 17:07:18 +0900

    word-list 추가

---
+++ src/app/search/search-header/word-disp-select.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { wordDispTypeInfo } from "@/constants/search";
+
+import { useWcStore } from "@/stores/wc-store";
+import { ChevronDown } from "lucide-react";
+export default function WordDispSelect() {
+  const wordDispType = useWcStore((e) => e.wordDispType);
+  const setWordDispType = useWcStore((e) => e.setWordDispType);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={"outline"}
+          className={"w-[100px] justify-between"}
+          size={"default"}
+        >
+          <div className="flex items-center gap-2">
+            <div className="font-normal">
+              {wordDispTypeInfo[wordDispType].title}
+            </div>
+          </div>
+          <ChevronDown className="size-3" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-50">
+        <DropdownMenuRadioGroup
+          value={`${wordDispType}`}
+          onValueChange={(e: string) => setWordDispType(Number(e))}
+        >
+          {wordDispTypeInfo.map(({ title }, i) => (
+            <DropdownMenuRadioItem value={`${i}`} key={i}>
+              {title}
+            </DropdownMenuRadioItem>
+          ))}
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
--- src/components/word-table/word-list.tsx
+++ src/components/word-table/word-list.tsx
@@ -1,61 +1,93 @@
+import { pageSizeInfo } from "@/constants/search";
 import { cn } from "@/lib/utils";
 import { useWcStore } from "@/stores/wc-store";
 import type { MoveRow } from "@/types/search";
 import { Minus } from "lucide-react";
+import { useState } from "react";
 import { Button, buttonVariants } from "../ui/button";
+import { PaginationSimple } from "../ui/pagination-simple";
+import { Separator } from "../ui/separator";
 
 export default function WordList({ rows }: { rows: MoveRow[] }) {
+  const [page, setPage] = useState(1);
+  const pageSize = useWcStore((e) => e.pageSize);
+
+  const totalPages = Math.ceil(rows.length / pageSizeInfo[pageSize].value);
+  const start = (page - 1) * pageSizeInfo[pageSize].value;
+  const currentRows = rows.slice(start, start + pageSizeInfo[pageSize].value);
+
   const addExceptedWord = useWcStore((e) => e.addExceptedWord);
   const search = useWcStore((e) => e.search);
   const autoSearch = useWcStore((e) => e.autoSearch);
   return (
-    <div className="flex flex-wrap items-center justify-center gap-2">
-      {rows.map((moveRow: MoveRow) =>
-        moveRow.words.map((e) => (
-          <div className="flex gap-[1.5px]">
+    <div className="flex flex-col gap-4">
+      {totalPages > 1 && (
+        <PaginationSimple
+          page={page}
+          totalPages={totalPages}
+          onPageChange={setPage}
+        />
+      )}
+      <div className="flex flex-wrap items-center justify-center gap-1 py-2">
+        {currentRows.map((moveRow: MoveRow) =>
+          moveRow.words.map((e) => (
             <div
               className={cn(
                 buttonVariants({
                   variant: "secondary",
                   size: "sm",
                 }),
-                "h-auto items-center gap-0 rounded-l-4xl rounded-r-xs px-0!",
+                "flex h-auto gap-0 px-0 py-0",
               )}
             >
-              {e.split("").map((char, i) => (
-                <Button
-                  key={i}
-                  onClick={() => {
-                    search(e);
-                  }}
-                  className={cn(
-                    "hover:text-muted-foreground h-8 p-0 hover:bg-transparent dark:hover:bg-transparent",
-                    {
-                      "pl-3": i === 0,
-                      "pr-2": i === e.length - 1,
-                    },
-                  )}
-                  variant={"ghost"}
-                >
-                  {char}
-                </Button>
-              ))}
+              <div>
+                {e.split("").map((char, i) => (
+                  <Button
+                    key={i}
+                    onClick={() => {
+                      search(char);
+                    }}
+                    className={cn(
+                      "hover:text-muted-foreground h-8 p-0 hover:bg-transparent dark:hover:bg-transparent",
+                      {
+                        "pl-2.5": i === 0,
+                        "pr-2": i === e.length - 1,
+                      },
+                    )}
+                    variant={"ghost"}
+                  >
+                    {char}
+                  </Button>
+                ))}
+              </div>
+              <Separator
+                className="bg-background h-8!"
+                orientation="vertical"
+              />
+              <Button
+                variant="ghost"
+                size="sm"
+                className="group/except-button pl-2!"
+                onClick={() => {
+                  addExceptedWord(e);
+                  if (autoSearch) {
+                    search(moveRow.move[1]);
+                  }
+                }}
+              >
+                <Minus className="stroke-muted-foreground group-hover/except-button:stroke-foreground size-4" />
+              </Button>
             </div>
-            <Button
-              variant="secondary"
-              size="sm"
-              className="rounded-l-xs rounded-r-4xl pl-2!"
-              onClick={() => {
-                addExceptedWord(e);
-                if (autoSearch) {
-                  search(moveRow.move[1]);
-                }
-              }}
-            >
-              <Minus className="stroke-muted-foreground size-4" />
-            </Button>
-          </div>
-        )),
+          )),
+        )}
+      </div>
+
+      {totalPages > 1 && (
+        <PaginationSimple
+          page={page}
+          totalPages={totalPages}
+          onPageChange={setPage}
+        />
       )}
     </div>
   );
