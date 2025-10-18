a0c64538e57183774910530798e9c260c59be71b
singrum <miamiq0000@gmail.com>
2025-10-19 05:28:23 +0900

    fix : 채팅 리스트 스크롤 버그 수정

--- src/app/play/play.tsx
+++ src/app/play/play.tsx
@@ -7,7 +7,7 @@ export default function Play() {
   const selectedGame = useWcStore((e) => e.selectedGame);
 
   return (
-    <div className="flex h-full max-h-[100dvh] w-full min-w-0 flex-col">
+    <div className="flex h-full max-h-[calc(100dvh-var(--header-height))] w-full min-w-0 flex-col">
       {selectedGame === null ? <GameSettings /> : <Game id={selectedGame} />}
       <MobilePlayDrawer />
     </div>
