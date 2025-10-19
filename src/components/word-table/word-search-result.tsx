82107728f2846dbe641dafc443820ab40839ab60
singrum <miamiq0000@gmail.com>
2025-10-19 23:08:57 +0900

    fix : data 변화에 따른 아코디온 open 상태 업데이트

--- src/components/word-table/word-search-result.tsx
+++ src/components/word-table/word-search-result.tsx
@@ -1,6 +1,6 @@
 import { useWcStore } from "@/stores/wc-store";
 import type { WordsCard } from "@/types/search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Accordion } from "../ui/accordion";
 import WordsCardComponent from "./words-card-component";
 
@@ -8,16 +8,23 @@ export default function WordSearchResult({ data }: { data: WordsCard[] }) {
   const searchInputValue = useWcStore((e) => e.searchInputValue);
   const view = useWcStore((e) => e.view);
   const defaultAllOpen = useWcStore((e) => e.defaultAllOpen);
+
   const defaultValue = !defaultAllOpen ? [`0`] : data.map((_, i) => `${i}`);
-  
+
   const [openValues, setOpenValues] = useState<string[]>(defaultValue);
-  
+
+  useEffect(() => {
+    const getDefaultValue = () =>
+      !defaultAllOpen ? [`0`] : data.map((_, i) => `${i}`);
+    setOpenValues(getDefaultValue());
+  }, [data, defaultAllOpen]);
+
   return (
     <Accordion
       key={`${view}-${searchInputValue}`}
       type="multiple"
-      defaultValue={defaultValue}
       className="space-y-2"
+      defaultValue={defaultValue}
       onValueChange={setOpenValues}
       value={openValues}
     >
