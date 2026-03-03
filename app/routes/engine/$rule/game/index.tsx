eb63fccd7f984c22b5a26809bd1fca8a794938e1
singrum <miamiq0000@gmail.com>
2026-03-03 21:44:56 +0900

    x

--- app/routes/engine/$rule/game/index.tsx
+++ app/routes/engine/$rule/game/index.tsx
@@ -1,13 +1,13 @@
-import { useIsMobile } from "~/hooks/use-mobile";
+import { useIsTablet } from "~/hooks/use-tablet";
 import Play from "~/routes/engine/$rule/game/+components/play";
 import PlaySidebar from "~/routes/engine/$rule/game/+components/play-sidebar/play-sidebar";
 import InnerSidebar from "../+components/inner-sidebar/inner-sidebar";
 
 export default function Game() {
-  const isMobile = useIsMobile();
+  const isTablet = useIsTablet();
   return (
     <div className="flex h-full">
-      {!isMobile && (
+      {!isTablet && (
         <InnerSidebar>
           <PlaySidebar />
         </InnerSidebar>
