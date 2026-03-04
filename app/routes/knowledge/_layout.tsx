06166d65b6605b57583f8e18e5ce4adad062ae7e
singrum <miamiq0000@gmail.com>
2026-03-04 16:25:34 +0900

    지식 탭 bg-background 적용

--- app/routes/knowledge/_layout.tsx
+++ app/routes/knowledge/_layout.tsx
@@ -1,14 +1,24 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router";
 import Header from "./+components/header";
 import Sidebar from "./+components/sidebar";
 
 export default function KnowledgeLayout() {
+  useEffect(() => {
+    const originalStyle = window.getComputedStyle(
+      document.body,
+    ).backgroundColor;
+    document.body.style.backgroundColor = "var(--background)";
+
+    return () => {
+      document.body.style.backgroundColor = originalStyle;
+    };
+  }, []);
   return (
-    <div>
+    <div className="bg-background">
       <Header />
       <div className="flex bg-background">
         <Sidebar />
-
         <div className="flex-1">
           <Outlet />
         </div>
