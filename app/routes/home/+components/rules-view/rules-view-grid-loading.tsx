0792a8b4c26f45b2aa0db4f5c634949899a234a2
singrum <miamiq0000@gmail.com>
2026-03-04 18:19:05 +0900

    x

--- app/routes/home/+components/rules-view/rules-view-grid-loading.tsx
+++ app/routes/home/+components/rules-view/rules-view-grid-loading.tsx
@@ -4,8 +4,8 @@ import { Skeleton } from "~/components/ui/skeleton";
 export default function RulesViewGridLoading() {
   return (
     <div className="grid grid-cols-[repeat(auto-fill,minmax(180px,1fr))] gap-4 px-6 pb-6">
-      {range(20).map(() => (
-        <Skeleton className="h-13 w-full" />
+      {range(20).map((i) => (
+        <Skeleton className="h-13 w-full" key={i} />
       ))}
     </div>
   );
