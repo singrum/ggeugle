8464ec345a959d42c946a2a849c68080c2cb8fb0
singrum <miamiq0000@gmail.com>
2026-03-04 18:18:14 +0900

    태블릿 로딩 헤더 스타일 불일치 수정

--- app/routes/engine/$rule/search/+components/engine-loading.tsx
+++ app/routes/engine/$rule/search/+components/engine-loading.tsx
@@ -8,8 +8,8 @@ export default function EngineLoading() {
   return (
     <div className="lg:bg-sidebar flex flex-col lg:h-svh min-h-dvh lg:min-h-0">
       <div className="flex-1 min-h-0 flex flex-col lg:relative ">
-        <header className="shrink-0 flex items-center justify-between h-14 pl-4 md:pl-6 pr-2 bg-sidebar border-b dark:border-0 lg:border-0 lg:dark:border-0">
-          <div className="flex items-center gap-4 md:gap-6 min-w-0">
+        <header className="shrink-0 flex items-center justify-between h-14 pl-4 lg:pl-6 pr-2 bg-sidebar border-b dark:border-0 lg:border-0 lg:dark:border-0">
+          <div className="flex items-center gap-4 md:gap-6 min-w-0 ">
             <Link to="/home" className="-m-6 p-6">
               <IkkiLogo className="h-3.5 lg:h-4 w-auto " />
             </Link>
