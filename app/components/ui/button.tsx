95c48b46062b89c4adc955e51956e1095ea5d80c
singrum <miamiq0000@gmail.com>
2026-03-17 10:30:05 +0900

    button destructive variant 수정

--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -11,12 +11,11 @@ const buttonVariants = cva(
       variant: {
         default: "bg-primary text-primary-foreground  hover:bg-primary/90",
         destructive:
-          "bg-destructive text-white  hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+          "bg-destructive/10 hover:bg-destructive/20 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/20 text-destructive focus-visible:border-destructive/40 dark:hover:bg-destructive/30",
         outline:
-          "border bg-transparent  hover:bg-accent hover:text-accent-foreground  dark:border-input dark:hover:bg-input/50",
+          "border bg-transparent  hover:bg-accent hover:text-accent-foreground dark:border-input dark:hover:bg-input/50",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline text-foreground",
