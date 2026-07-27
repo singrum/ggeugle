89f2a1add61ccf628219ec08284f56a313ff677c
singrum <miamiq0000@gmail.com>
2026-07-28 08:53:30 +0900

    .

--- app/components/ads/banner-ads.tsx
+++ app/components/ads/banner-ads.tsx
@@ -1,15 +1,18 @@
 import { Adsense } from "@ctrl/react-adsense";
+import { useIsMobile } from "~/hooks/use-mobile";
 
 export default function BannerAds() {
+  const isMobile = useIsMobile();
   return (
     <div className="w-full flex justify-center">
       <Adsense
         client="ca-pub-3218283453997693"
         slot="1024680318"
         style={{
-          width: "728px",
-          height: "90px",
           display: "inline-block",
+          ...(isMobile
+            ? { width: "100%" }
+            : { width: "728px", height: "90px" }),
         }}
         format="fluid"
         responsive="true"
