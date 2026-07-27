376c404af8cd43d974942a9b364b16ab00d6c1ea
singrum <miamiq0000@gmail.com>
2026-07-28 02:02:10 +0900

    .

--- app/routes/engine/$rule/search/+components/search-page.tsx
+++ app/routes/engine/$rule/search/+components/search-page.tsx
@@ -14,7 +14,7 @@ export default function SearchPage() {
           <Adsense
             client="ca-pub-3218283453997693"
             slot="1024680318"
-            style={{ display: "block" }}
+            style={{ width: "100%", display: "block" }}
             format="fluid"
           />
         </div>
