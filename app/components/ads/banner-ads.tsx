660513557f44f299fe1c1dbd749306878ffeb804
singrum <miamiq0000@gmail.com>
2026-08-07 22:20:10 +0900

    .

--- app/components/ads/banner-ads.tsx
+++ app/components/ads/banner-ads.tsx
@@ -11,8 +11,8 @@ export default function BannerAds() {
         style={{
           display: "inline-block",
           ...(isMobile
-            ? { width: "100%", height: "90px" }
-            : { width: "728px", height: "90px" }),
+            ? { width: "100%", height: "108px" }
+            : { width: "728px", height: "108px" }),
         }}
         format="fluid"
         responsive="true"
